63178a2cfb361c7d5b64c9d13bae0f7f22bcef6f
MondayFeb <minhthu2nd.contact@gmail.com>
2024-03-26 22:01:17 +0700

    add design TCPChat

---
+++ TCP2/TCPChat/TCPChat/TCPClient.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TCPChat
+{
+    partial class TCPClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIPAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbWaiting = new System.Windows.Forms.TextBox();
+            this.tbMessages = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // tbIPAddress
+            // 
+            this.tbIPAddress.Location = new System.Drawing.Point(158, 39);
+            this.tbIPAddress.Name = "tbIPAddress";
+            this.tbIPAddress.Size = new System.Drawing.Size(281, 26);
+            this.tbIPAddress.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(562, 46);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(117, 26);
+            this.tbPort.TabIndex = 3;
+            // 
+            // tbWaiting
+            // 
+            this.tbWaiting.Location = new System.Drawing.Point(55, 134);
+            this.tbWaiting.Multiline = true;
+            this.tbWaiting.Name = "tbWaiting";
+            this.tbWaiting.Size = new System.Drawing.Size(623, 65);
+            this.tbWaiting.TabIndex = 4;
+            // 
+            // tbMessages
+            // 
+            this.tbMessages.Location = new System.Drawing.Point(63, 244);
+            this.tbMessages.Name = "tbMessages";
+            this.tbMessages.Size = new System.Drawing.Size(615, 190);
+            this.tbMessages.TabIndex = 5;
+            this.tbMessages.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(298, 81);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(141, 47);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(298, 470);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(141, 47);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // TCPClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 548);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbMessages);
+            this.Controls.Add(this.tbWaiting);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbIPAddress);
+            this.Controls.Add(this.label1);
+            this.Name = "TCPClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIPAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbWaiting;
+        private System.Windows.Forms.RichTextBox tbMessages;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
